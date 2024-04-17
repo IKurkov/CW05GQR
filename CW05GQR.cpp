ff99286b422118fd738cc33a0b9da4f6016274c6
IKurkov <Ivan.Kurkov@list.ru>
2024-04-17 15:55:04 +0300

    All work

--- CW05GQR.cpp
+++ CW05GQR.cpp
@@ -26,7 +26,8 @@ int main( void )
 {
   bool run = true;
   size_t n;
-  double moments[4], nodes[2];
+  int key, FId;
+  double moments[4], nodes[2], a0, a1, det, I1, I2;
   AcmFVals acm = Accumulate(Weight, ITER, a, b);
   IQR Rule;
 
@@ -43,14 +44,19 @@ int main( void )
       "0 - exit\n"
       "1 - interpolation quadrature rules\n"
       "2 - Gaussian quadrature rules\n";
-    switch (_getch())
-    {
-    case '0':
+    key = _getch();
+
+    if (key == '0')
       run = false;
-      break;
-    case '1':
+    else if (key == '1' || key == '2')
     {
       fort::char_table stats;
+
+      std::cout << "Choose function to integrate:\n";
+      for (size_t i = 0; i < NumOfFunctions; i++)
+        std::cout << i << " - f(x) = " << FunctionsList[i].Expr << '\n';
+      while ((FId = _getch() - '0') < 0 || FId >= NumOfFunctions)
+        std::cout << "[Error]: Incorrect choice, try again!\n";
 
       std::cout << "Input number of nodes (= 2): ";
       std::cin >> n;
@@ -60,28 +66,43 @@ int main( void )
         std::cout << "Input number of nodes (= 2): ";
         std::cin >> n;
       }
-      do
+      if (key == '1')
+        do
+        {
+          std::cout << "Input 2 different nodes, integration interval = (0, 1): ";
+          std::cin >> nodes[0] >> nodes[1];
+        } while (nodes[0] == nodes[1]);
+      else
       {
-        std::cout << "Input 2 different nodes, integration interval = (0, 1): ";
-        std::cin >> nodes[0] >> nodes[1];
-      } while (nodes[0] == nodes[1]);
+        det = moments[1] * moments[1] - moments[0] * moments[2];
+        a0 = (moments[2] * moments[2] - moments[1] * moments[3]) / det;
+        a1 = (moments[0] * moments[3] - moments[1] * moments[2]) / det;
+        nodes[0] = (-a1 - sqrt(a1 * a1 - 4 * a0)) / 2;
+        nodes[1] = (-a1 + sqrt(a1 * a1 - 4 * a0)) / 2;
+      }
 
       Rule = IQR(nodes, moments, n);
-      acm = Accumulate([]( double x ){ return Weight(x) * Variant7(x); }, ITER, a, b);
+      acm = Accumulate([FId]( double x ){ return Weight(x) * FunctionsList[FId].Func(x); }, ITER, a, b);
 
       stats << fort::header << 'i' << "x_i" << "m_i" << "A_i" << fort::endr;
       for (size_t i = 0; i < n; i++)
         stats << i << nodes[i] << moments[i] << Rule.Coeffs[i] << fort::endr;
       std::cout << stats.to_string();
-      std::cout << "I_[ * ](" << WeightStr << '*' << FunctionsList[4].Expr << ", " << a << ", " << b  << ") = "
-        << QRMiddleRectangles(acm) << '\n';
-      std::cout << "I_IQR(" << WeightStr << '*' << FunctionsList[4].Expr << ", " << a << ", " << b << ") = "
-        << Rule.Integrate(Variant7) << '\n';
-      break;
+      if (key == '2')
+        std::cout << "Orthogonal polynomial: w(x) = x^2"
+          << (a1 < 0 ? " - " : " + ") << fabs(a1)
+          << "x" << (a0 < 0 ? " - " : " + ") << fabs(a0) << '\n';
+      I1 = QRMiddleRectangles(acm);
+      I2 = Rule.Integrate(FunctionsList[FId].Func);
+      std::cout << "I_[ * ](" << WeightStr << " * [" << FunctionsList[FId].Expr << "], " << a << ", " << b  << ") = "
+        << std::setprecision(15) << I1 << '\n';
+      std::cout << "I_" << (key == '1' ? 'I' : 'G') << "QR("
+        << WeightStr << " * [" << FunctionsList[FId].Expr << "], " << a << ", " << b << ") = "
+        << std::setprecision(15) << I2 << '\n';
+      std::cout << "Delta: " << fabs(I1 - I2) << '\n';
     }
-    default:
+    else
       std::cout << "[Error]: Incorrect choice!\n";
-    }
   }
   return 0;
 }
